a16de329edf88a4221df2e81b316e6f77e1ccc13
Odaawa <odaawa77@gmail.com>
2019-08-22 23:32:24 +0100

    Added comments, synced css for all pages

--- js_files/int.js
+++ js_files/int.js
@@ -1,12 +1,12 @@
 	const p = document.getElementById('principal').value;
-    const t = document.getElementById('time').value;
-    const r = document.getElementById('rate').value;
+  const t = document.getElementById('time').value;
+  const r = document.getElementById('rate').value;
     //n = compounding periods
     const n = document.getElementById('compound').value;
     let totalAmount = document.getElementById('displayAmount').value;
     let interest =document.getElementById('displayInterest').value;
 
-//compound interest
+// calculate compound interest
 const compound = function(p, t, r, n){
 	const result = 1 + (r/n);
 	const answer = Math.pow(result, t*n);
@@ -16,8 +16,18 @@ const compound = function(p, t, r, n){
 }
 
 
-//simple interest
+// calculate simple interest
 const simpleInterest = function(p, t, r){
    const  s_interest = p*t*r;
    const amount = p + simpleInterest;
+}
+
+//clear input areas
+function clear_display(){
+  document.getElementById('principal').value = "";
+  document.getElementById('time').value = "";
+  document.getElementById('rate').value = "";
+  document.getElementById('compound').value = "";
+  totalAmount = "";
+  interest = "";
 }
