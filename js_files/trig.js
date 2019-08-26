6ffca00abeeca0d71fdf4c54875ca97d784dd071
Odaawa <odaawa77@gmail.com>
2019-08-26 15:49:12 +0100

    changed function to arrow functions

--- js_files/trig.js
+++ js_files/trig.js
@@ -1,110 +1,31 @@
+//for the number buttons
+run1 = () => {document.case.display.value += "1"};
+run2 = () => {document.case.display.value += "2"};
+run3 = () => {document.case.display.value += "3"};
+run4 = () => {document.case.display.value += "4"};
+run5 = () => {document.case.display.value += "5"};
+run6 = () => {document.case.display.value += "6"};
+run7 = () => {document.case.display.value += "7"};
+run8 = () => {document.case.display.value += "8"};
+run9 = () => {document.case.display.value += "9"};
+run0 = () => {document.case.display.value += "0"};
 
-
-function run1(){
-
-	document.case.display.value += "1"  //+= means append to, so that value is added on to whatever is already on display
-};
-function run2(){
-
-	document.case.display.value += "2"
-};
-function run3(){
-
-	document.case.display.value += "3"
-};
-function run4(){
-
-	document.case.display.value += "4"
-};
-function run5(){
-
-	document.case.display.value += "5"
-};
-
-function run6(){
-
-	document.case.display.value += "6"
-};
-
-function run7(){
-
-	document.case.display.value += "7"
-};
-
-function run8(){
-
-	document.case.display.value += "8"
-};
-
-function run9(){
-
-	document.case.display.value += "9"
-};
-
-function run0(){
-
-	document.case.display.value += "0"
-};
-
-function degrees(){
-    document.case.display.value =  document.case.display.value * (Math.PI/180);
-};
-
-
-function runC(){
-
-	document.case.display.value = ""
-};
-function runDot(){
-
-	document.case.display.value += "."
-};
-function runMinus(){
-
-	document.case.display.value = "-"
-};
-
-
+runDot = () => {document.case.display.value += "."};
+runC = () => {document.case.display.value = ""}; //clear function
+runMinus = () => {document.case.display.value += "-"};
+degrees = () => {document.case.display.value =  document.case.display.value * (Math.PI/180)};
 
 //for the trig operations 
-function runSin(){
-	document.case.display.value = Math.sin(document.case.display.value);
+runSin = () => {document.case.display.value = Math.sin(document.case.display.value)};
+runCos = () => {document.case.display.value = Math.cos(document.case.display.value)};
+runTan = () => {document.case.display.value = Math.tan(document.case.display.value)};
+runArcSin = () => {document.case.display.value = Math.asin(document.case.display.value)};
+runArcCos = () => {document.case.display.value = Math.acos(document.case.display.value)};
+runArcTan = () => {document.case.display.value = Math.atan(document.case.display.value)};
+runSinh = () => {document.case.display.value = Math.sinh(document.case.display.value)};
+runCosh = () => {document.case.display.value = Math.cosh(document.case.display.value)};
+runTanh = () => {document.case.display.value = Math.tanh(document.case.display.value)};
 	
-}
-
-
-function runCos(){
-	document.case.display.value = Math.cos(document.case.display.value);
-}
-
-function runTan(){
-	document.case.display.value = Math.tan(document.case.display.value);
-}
-
-function runArcSin(){
-	document.case.display.value = Math.asin(document.case.display.value);
-}
-
-function runArcCos(){
-	document.case.display.value = Math.acos(document.case.display.value);
-}
-
-function runArcTan(){
-	document.case.display.value = Math.atan(document.case.display.value);
-}
-
-function runSinh(){
-	document.case.display.value = Math.sinh(document.case.display.value);
-}
-
-function runCosh(){
-	document.case.display.value = Math.cosh(document.case.display.value);
-}
-
-function runTanh(){
-	document.case.display.value = Math.tanh(document.case.display.value);
-}
-
 
 function runBackspace() {
     var erase = document.getElementById("display").value;
