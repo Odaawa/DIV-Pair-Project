ca343502fcecfe40c8addb25f60835968ffb920d
Odaawa <odaawa77@gmail.com>
2019-08-26 23:27:58 +0100

    fixed compound options

--- js_files/int.js
+++ js_files/int.js
@@ -2,47 +2,49 @@
   var t = document.getElementById('time').value;
   var rate = document.getElementById('rate').value;
   var r = rate/100;
-    //n = compounding periods
-    // var compoundingRate = document.getElementById('compound_options').value;//find out how best to do this
-    // var n = compoundingRate.options[compoundingRate.selectedIndex].value;//accessing value from select menu
 
-//how to get js to access value from compounding rate options and work with it
-   // var compound_rate = document.querySelector('#compound_options');
-   // compound_rate.addEventListener('change', getCompoundingRate(){
-   //  return compound_rate.value;
-   // });
-   
-   
+  let totalAmount = document.getElementById('displayAmount');
+  let interest =document.getElementById('displayInterest');
 
-// function setWeather(){
-//  var choice = select.value;
+//swap between simple interest and compound interest
+// function answer(){
+//   if(document.querySelector('#interest_options').value === 'compoundInterest'){
+//     compound();
+//   }else if(document.querySelector('#interest_options').value === 'simpleInterest'){
+//     simple();
+//   }
+// }//check this out, sth's not working right, remember you called answer() in html so check that too
 
-   let totalAmount = document.getElementById('displayAmount');
-   let interest =document.getElementById('displayInterest');
+// function swap(){
+//   let option_1 = document.getElementById('compound');
+//   let option_2 = document.getElementById('simple');
+//   let button = document.getElementById('get_results');
 
-//swap between simple interest and compound interest 
-function swap(){
-  let option_1 = document.getElementById('compound');
-  let option_2 = document.getElementById('simple');
-  let button = document.getElementById('get_results');
-
-  option_1.addEventListener('click', () => {
-      document.getElementById('compound_options').style.opacity = 1;
-      document.getElementById('optionLabel_2').style.opacity = 1;
-      button.onclick = compound(); 
-  });
+//   option_1.addEventListener('click', () => {
+//       document.getElementById('compound_options').style.opacity = 1;
+//       document.getElementById('optionLabel_2').style.opacity = 1;
+//       button.onclick = compound(); 
+//   });
   
-  option_2.addEventListener('click', () => {
-      document.getElementById('compound_options').style.opacity = 0;
-      document.getElementById('optionLabel_2').style.opacity = 0;
-      button.onclick =simpleInterest();
-  });
-}
-swap();
+//   option_2.addEventListener('click', () => {
+//       document.getElementById('compound_options').style.opacity = 0;
+//       document.getElementById('optionLabel_2').style.opacity = 0;
+//       button.onclick =simpleInterest();
+//   });
+// }
+// swap();
+
+
+//for the compounding options selector
+const select_compound = document.querySelector('#compound_options');
+select_compound.addEventListener('change', get_n);//whenever we change the select option the value of n changes
+function get_n(){
+  return select_compound.value;
+};
 
 // calculate compound interest
 const compound = function(){
-  const n = getCompoundingRate();
+   const n = get_n();
   const sum = (r/n) + 1;
   const exponent = t*n;
   const sum_exponent = Math.pow(sum, exponent);
@@ -53,7 +55,7 @@ const compound = function(){
 }
 
 //calculate simple interest
-const simpleInterest = function(){
+const simple = function(){
    const  s_interest = p*t*r;
    const amount = p * (1 + (t*r));
    document.getElementById('displayAmount').value = amount;//.toFixed(2);
