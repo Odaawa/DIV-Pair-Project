5ab8c7dac3dcf50522c544349d1d7653a3a80d2e
Odaawa <odaawa77@gmail.com>
2019-08-26 13:44:17 +0100

    Changed functions to arrow functions

--- js_files/index.js
+++ js_files/index.js
@@ -1,91 +1,27 @@
-function run1(){
 
-	document.case.display.value += "1"  //+= means append to, so that value is added on to whatever is already on display
-};
-function run2(){
+//for the number buttons
+run1 = () => {document.case.display.value += "1"};
+run2 = () => {document.case.display.value += "2"};
+run3 = () => {document.case.display.value += "3"};
+run4 = () => {document.case.display.value += "4"};
+run5 = () => {document.case.display.value += "5"};
+run6 = () => {document.case.display.value += "6"};
+run7 = () => {document.case.display.value += "7"};
+run8 = () => {document.case.display.value += "8"};
+run9 = () => {document.case.display.value += "9"};
+run0 = () => {document.case.display.value += "0"};
 
-	document.case.display.value += "2"
-};
-function run3(){
+//for the operation buttons
+runPlus = () => {document.case.display.value += "+"};
+runMinus = () => {document.case.display.value += "-"};
+runDivide = () => {document.case.display.value += "/"};
+runMultiply = () => {document.case.display.value += "*"};
 
-	document.case.display.value += "3"
-};
-function run4(){
-
-	document.case.display.value += "4"
-};
-function run5(){
-
-	document.case.display.value += "5"
-};
-
-function run6(){
-
-	document.case.display.value += "6"
-};
-
-function run7(){
-
-	document.case.display.value += "7"
-};
-
-function run8(){
-
-	document.case.display.value += "8"
-};
-
-function run9(){
-
-	document.case.display.value += "9"
-};
-
-function run0(){
-
-	document.case.display.value += "0"
-};
-function runPlus(){
-
-	document.case.display.value += "+"
-};
-function runMinus(){
-
-	document.case.display.value += "-"
-};
-function runDivide(){
-
-	document.case.display.value += "/"
-};
-function runMultiply(){
-
-	document.case.display.value += "*"
-};
-
-function runC(){
-
-	document.case.display.value = ""
-};
-function runDot(){
-
-	document.case.display.value += "."
-};
-
-function runOpenBracket(){
-
-	document.case.display.value += "("
-};
-
-function runCloseBracket(){
-	document.case.display.value += ")"
-};
-
-/*The eval() function evaluates or executes an argument.
-If the argument is an expression, eval() evaluates the expression. If the argument is one or more JavaScript statements, eval() executes the statements.*/
-function runEquals() {
-	var equals = eval(document.case.display.value)
-	document.case.display.value = equals;
-}
-
-
+//other  buttons (),.
+runOpenBracket = () => {document.case.display.value += "("};
+runrunCloseBracket = () => {document.case.display.value += ")"};
+runDot = () => {document.case.display.value += "."};
+runC = () => {document.case.display.value = ""}; //clear function
 
 function runBackspace() {
     var erase = document.getElementById("display").value;
@@ -93,4 +29,9 @@ function runBackspace() {
 }
 //substr(start point, number of characters to be returned) - produces a substring out of the initial string 
 
-
+/*The eval() function evaluates or executes an argument.
+If the argument is an expression, eval() evaluates the expression. If the argument is one or more JavaScript statements, eval() executes the statements.*/
+function runEquals() {
+	var equals = eval(document.case.display.value)
+	document.case.display.value = equals;
+}
