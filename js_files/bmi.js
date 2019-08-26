dbe5d0c81f676e5ab863040c8e880bec2c897efb
Odaawa <odaawa77@gmail.com>
2019-08-26 23:30:24 +0100

    Add something small

--- js_files/bmi.js
+++ js_files/bmi.js
@@ -4,6 +4,32 @@ const bmi_calc = function(mass, height){
 	const result= mass/Math.pow(height,2);
 	const bmi_index = result.toFixed(1);//toFixed() method reduces number of dp to what is specified in the brackets
 
+
+var input_mass =document.getElementById('weight_kg');
+var input_height =document.getElementById('height_m');
+
+
+//form validation using blur and focus
+// input_mass.onblur = function(){
+// 	if(typeof mass !== 'number'){
+// 		input.classList.add('invalid');
+// 		error.innerHTML = 'Input must be a number!';
+// 	}
+// }
+// 	// if(typeof height !== 'number'){
+// 	// 	input.classList.add('invalid');
+// 	// 	error.innerHTML = 'Input must be a number!';
+// 	// } 	
+// // 
+// input_mass.onfocus = function(){
+// 	if(this.classList.contains('invalid')){
+// 		this.classList.remove('invalid');
+// 		error.innerHTML = '';
+// 	}
+// }
+
+
+
 //this would require js to look through html file, identify display_1 under bmi and assign bmi_index to value
 //user would then see bmi_index value in display area 1
     document.bmi.display_1.value = bmi_index;
